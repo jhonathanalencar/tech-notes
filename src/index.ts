a6c68bd795099dfc4253f5536e228a25557ba052
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-01-24 15:43:27 -0300

    feat: add request logger middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import path from 'node:path';
 
+import { logger } from './middlewares/logger';
+
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 
 app.use('/', express.static(path.resolve(__dirname, '..', 'public')));
 
---
+++ src/middlewares/logger.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { logEvents } from '../utils/logEvents';
+
+function logger(request: Request, response: Response, next: NextFunction) {
+  const message = `${request.method}\t${request.url}\t${request.headers.origin}`;
+  logEvents(message, 'requestLog.log');
+
+  console.log(`${request.method} ${request.path}`);
+  next();
+}
+
+export { logger };
